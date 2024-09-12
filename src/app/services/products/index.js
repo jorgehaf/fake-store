d16be9d3b5b6e414a6dc505bf9e4ae1a5316e26b
Jorge Ferreira <jorge.ferreira@vitta.me>
2024-09-12 16:03:56 -0300

    nova rota para products

--- src/app/services/products/index.js
+++ src/app/services/products/index.js
@@ -1,6 +1,9 @@
 import client from "../config/client";
 
 let products = {
+    getSingleProduct: (idProduct) => {
+        return client.get(`/products/${idProduct}`);
+    },
     getProducts: (params) => {
         return client.get("/products", params);
     },
