bf904e4ce075b4b7bbf06b8f0e6352e0637d502b
Jorge Ferreira <jorge.ferreira@vitta.me>
2024-09-14 15:45:43 -0300

    Retirada de arquivo desnecessários

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,15 +1,56 @@
 "use client";
 
-import ProductIndex from "./index";
-import { Provider } from 'react-redux';
-import { store } from '@/store/store';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import styles from './styles';
+import services from '@/services';
 
-const ProductDetails = ({ params }) => {
+import CartInfo from '@/components/CartInfo';
+import AddCartButton from '@/components/AddCartButton';
+import LoadingLayer from '@/components/LoadingLayer';
+
+export default function ProductDetails({ params }) {
+    const [product, setProduct] = useState(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        const fetchProductDetails = async () => {
+            try {
+                const response = await services.products.getSingleProduct(params.id);
+                setProduct(response.product);
+            } catch (error) {
+                alert("Erro na busca pelo produto. Tente novamente!");
+            }
+        };
+
+        fetchProductDetails();
+    }, [params]);
+
+    if (!product) {
+        return <LoadingLayer />;
+    }
+
     return (
-        <Provider store={store}>
-            <ProductIndex params={params} />
-        </Provider>
-    );
-};
+        <styles.Container>
+            <styles.ContainerHeader>
+                <styles.BackButton onClick={() => router.back()}>Voltar</styles.BackButton>
+                <CartInfo />
+            </styles.ContainerHeader>
 
-export default ProductDetails;
+            <styles.ProductGrid>
+                <styles.ProductImage src={product.image} alt={product.title} />
+
+                <styles.ProductInfo>
+                    <styles.ProductName>{product.title}</styles.ProductName>
+                    <styles.ProductCategory>Categoria: {product.category}</styles.ProductCategory>
+                    <styles.ProductDescription>{product.description}</styles.ProductDescription>
+                </styles.ProductInfo>
+
+                <styles.ProductPurchase>
+                    <styles.ProductPrice>R$ {product.price.toFixed(2)}</styles.ProductPrice>
+                    <AddCartButton product={product} />
+                </styles.ProductPurchase>
+            </styles.ProductGrid>
+        </styles.Container>
+    );
+}
